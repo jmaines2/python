dfee2214545fea5bdb08b4dd3fc6cbe7816c3917
James Maines <jmaines2@gmail.com>
2022-12-19 10:09:49 -0800

    Update app.py
    added routing

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask import jsonfiy
 app = Flask(__name__)
 
 def append(something)
-    res = something + "est"
+    res = something + ' new'
     return res
     
+@app.route('/')
+    def hello():
+        print("I can make something new")
+        return 'I can make something new. Type something at routh: /new'
+    
+@app.route('/new/<typed>')
+    def makeNew
+        print(f"Make it new")
+        result = append(typed)
+        return jsonify(result)
